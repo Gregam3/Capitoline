554dcd20a456a1637f8a49ca7bdb9bf8640e2e7e
Greg <gregoryamitten@gmail.com>
2018-01-27 17:54:44 +0000

    progress getting database integrated

---
+++ src/main/java/com/greg/configuration/JdbcConfiguration.java
@@ -0,0 +1,19 @@
+package com.greg.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JdbcConfiguration {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JdbcConfiguration(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    // ...
+
+}
